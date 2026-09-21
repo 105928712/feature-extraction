bdeee35066c468416c6e2f9f7574ea97a5e3c8a5
TheFlyingRat <contact@theflyingrat.com>
2026-09-21 16:21:48 +1000

    feat: upgrade class api

--- features/__init__.py
+++ features/__init__.py
@@ -1,4 +1,17 @@
+# __init__.py
 # 2026 Joey Manani & Anchorfish Team
 
 from .base import Feature
+from .entropy import Entropy, HostEntropy, PathEntropy, QueryEntropy
 from .scheme import Scheme
+from .utils import normalise_url
+
+FEATURES = [Scheme, Entropy, HostEntropy, PathEntropy, QueryEntropy]
+
+def extract_features(url: str) -> dict:
+    """Normalise a URL and run every feature on it."""
+    url = normalise_url(url)
+    return {f.name: f(url).value for f in FEATURES}
+
+# public api exports
+__all__ = ["Feature", "extract_features", *[f.__name__ for f in FEATURES]]
